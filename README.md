be7962ecb11a97e6bae29b778c0edbe45530a912
Wilson Ow <wilson@Wilsons-MacBook-Pro.local>
2020-01-06 14:11:39 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,76 @@
+ftctl
+=====
+
+CLI tool for minimal automation of Drone CD flow using Helm Chart type deployment.
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/ftctl.svg)](https://npmjs.org/package/ftctl)
+[![Downloads/week](https://img.shields.io/npm/dw/ftctl.svg)](https://npmjs.org/package/ftctl)
+[![License](https://img.shields.io/npm/l/ftctl.svg)](https://github.com/wilson_13/ftctl/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g ftctl
+$ ftctl COMMAND
+running command...
+$ ftctl (-v|--version|version)
+ftctl/1.0.2 darwin-x64 node-v12.12.0
+$ ftctl --help [COMMAND]
+USAGE
+  $ ftctl COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`ftctl help [COMMAND]`](#ftctl-help-command)
+* [`ftctl release-stage [STRING]`](#ftctl-release-stage-string)
+
+## `ftctl help [COMMAND]`
+
+display help for ftctl
+
+```
+USAGE
+  $ ftctl help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.3/src/commands/help.ts)_
+
+## `ftctl release-stage [STRING]`
+
+This command updates helm chart values.yaml (image.tag) and Chart.yaml (appVersion) to be the version provided as argument. 
+
+```
+USAGE
+  $ ftctl release-stage [STRING]
+
+OPTIONS
+  -h, --help             show CLI help
+  -p, --path=path        path to chart's root folder
+  -v, --version=version  version to build and release
+
+DESCRIPTION
+  This command updates helm chart values.yaml (image.tag) and Chart.yaml (appVersion) to be the version provided as 
+  argument. 
+  After updating these values, it also performs upload the new chart and execute git tag and push tag. Note: Chart 
+  version should be updated manually.
+
+EXAMPLE
+  $ ftctl release-stage -v 0.1.0-beta.1 -p ../your-chart/
+  App versioned '0.1.0-beta.1' build and releasing to staging initiated.
+```
+
+_See code: [src/commands/release-stage.ts](https://github.com/wilson_13/ftctl/blob/v1.0.2/src/commands/release-stage.ts)_
+<!-- commandsstop -->
---
+++ src/commands/release-stage.ts
@@ -0,0 +1,110 @@
+import {Command, flags} from '@oclif/command';
+import * as fs from 'fs';
+import * as nodePath from 'path';
+import * as yaml from 'js-yaml';
+import * as simplegit from 'simple-git/promise';
+import { spawn } from 'child_process';
+
+const ENCODING = 'utf8'; // string
+// const NEWLINE = '\n';
+// const EQUALS = '=';
+const REMOTE = process.env.REMOTE;
+
+/**
+ * Command for helping developers to trigger CI/CD pipeline.
+ * Step 1. Update Chart.yaml and values.yaml's appVersion and image.tag values.
+ * Step 2. Upload new chart to chartmuseum.
+ * Step 3. Execute git tag and git tag push to trigger Drone CI build.
+ * 
+ * Note: Chart version should be updated manually.
+ */
+export default class Release extends Command {
+  
+  static description = `
+  This command updates helm chart values.yaml (image.tag) and Chart.yaml (appVersion) to be the version provided as argument. 
+After updating these values, it also performs upload the new chart and execute git tag and push tag. Note: Chart version should be updated manually.`
+
+  static examples = [
+    `$ ftctl release-stage -v 0.1.0-beta.1 -p ../your-chart/
+App versioned '0.1.0-beta.1' build and releasing to staging initiated.
+`,
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-v, --version=VALUE)
+    version: flags.string({char: 'v', description: 'version to build and release'}),
+    // flag with a value (-p, --path=VALUE)
+    path: flags.string({char: 'p', description: 'path to chart\'s root folder'}),
+  }
+
+  static args = [{name: 'string'}];
+
+  async run() {
+
+    const {args, flags} = this.parse(Release);
+    const version:string = flags.version as string;
+    const git = simplegit();
+    
+    var path = flags.path;
+    
+    if (!version) {
+      this.log('Argument version (-v/--version) not found, aborting build trigger.')
+    }
+
+    if (!path) {
+      path = __dirname + '/Chart.yaml';
+    }
+
+    /** 
+     * Step 1. Update Chart.yaml and values.yaml's appVersion and image.tag values.
+     */
+    this.log('Updating charts...');
+    // Get Chart.yaml path
+    let chartPath = nodePath.resolve(process.cwd(), path + '/Chart.yaml');
+    let chartContent = fs.readFileSync(chartPath, ENCODING);
+    let chartYaml = yaml.safeLoad(chartContent);
+
+    // Update app version value with the version provided
+    chartYaml.appVersion = version;
+    // Serialise object
+    let chartYamlStr = yaml.safeDump(chartYaml);
+    // Save the file (Chart.yaml)
+    fs.writeFileSync(chartPath, chartYamlStr, ENCODING);
+
+    // Get values.yaml path
+    let valuesPath = nodePath.resolve(process.cwd(), path + '/values.yaml');
+    let valuesContent = fs.readFileSync(valuesPath, ENCODING);
+    let valuesYaml = yaml.safeLoad(valuesContent);
+
+    // Update image tag version value with the version provided
+    valuesYaml.image.tag = version;
+    // Serialise object
+    let valuesYamlStr = yaml.safeDump(valuesYaml);
+    // Save the file (values.yaml)
+    fs.writeFileSync(valuesPath, valuesYamlStr, ENCODING);
+    
+    /** 
+     * Step 2. Upload new chart to chartmuseum.
+     */
+    this.log('Uploading charts...');
+    const chartFolderPath = nodePath.resolve(process.cwd(), path);
+    spawn('helm', ['push', chartFolderPath, '-f', 'chartmuseum'], {
+      stdio: 'inherit' // Will use process .stdout, .stdin, .stderr
+    })
+
+    /**
+     * Step 3. Execute git tag and git tag push to trigger Drone CI build.
+     */
+    this.log('Git tagging and pushing...');
+    // Perform git tag
+    const tagResult = await git.addTag(version);
+    console.log(tagResult);
+     // Perform git push tag
+    const tagPushRes = await git.pushTags(REMOTE);
+    console.log(tagPushRes);
+
+    // Output about the build
+    this.log(`build and release ${version}`);
+  }
+}
